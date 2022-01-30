20494f313f2c5f814cc240c1e34a18386229f680
“zyhon96” <adedoyin27@gmail.com>
2022-01-30 20:56:44 +0100

    App fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
 import logo from "../images/sas.png";
-import { TransactionContext } from "../context/TransactionContext";
 const NavbarItem = ({ title, classProps }) => {
   return <li className={`mx-4 cursor-pointer ${classProps}`}>{
     title}
@@ -11,7 +10,6 @@ const NavbarItem = ({ title, classProps }) => {
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
-  const { currentAccount } = useContext(TransactionContext);
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -21,10 +19,9 @@ const Navbar = () => {
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavbarItem key={item + index} title={item} />
         ))}
-        { currentAccount &&
           <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]  ">
           Login
-        </li>}
+        </li>
       </ul>
       <div className="flex relative">
         {toggleMenu 
